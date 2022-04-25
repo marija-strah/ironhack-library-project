29fdd7f00fe0772812aba68779c613ab75aed034
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-04-25 16:14:43 +0200

    add book form

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,59 +1,59 @@
-// const Book = require("../models/Book.model");
-
-// const router = require("express").Router();
-
-// router.get("/books", (req, res, next) => {
-//     Book.find()
-//         .then( (booksArr) => {
-
-//           console.log("hello");
-//             console.log(booksArr)
-
-//             res.render("books/books-list", {books: booksArr});
-//         })
-//         .catch( err => {
-//             console.log("error getting books from DB", err)
-//             next(err);
-//         });
-// });
-
-
-// module.exports = router;
-
-
 const Book = require("../models/Book.model");
 
 const router = require("express").Router();
 
 router.get("/books", (req, res, next) => {
     Book.find()
-        .then( (booksArr) => {
-          console.log("hhiihi");
-          console.log(booksArr);
-            res.render("books/books-list", {books: booksArr});
+        .then((booksArr) => {
+            res.render("books/books-list", { books: booksArr });
         })
-        .catch( err => {
+        .catch(err => {
             console.log("error getting books from DB", err)
             next(err);
         });
 });
 
 
-router.get("/books/:bookId", (req, res, next) => {
-  const id = req.params.bookId;
+router.get("/books/create", (req, res, next) => {
+    res.render("books/book-create");
+})
 
-  Book.findById(id)
+
+router.post("/books/create", (req, res, next) => {
+  const newBook = {
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating,
+}
+
+Book.create(newBook)
+  .then((bookFromDB) => {
+    
+    res.redirect("/books");
+  })
+  .catch(err => {
+    console.log("error creating book on DB", err);
+    next(err);
+  });
+
+// res.send("processing post request");
+})
+
+
+router.get("/books/:bookId", (req, res, next) => {
+    const id = req.params.bookId;
+
+    Book.findById(id)
         .then((bookDetails) => {
-            console.log(bookDetails)
-            //res.send("success")
-            res.render("books/book-details", {book: bookDetails})
+            res.render("books/book-details", bookDetails);
         })
         .catch(err => {
-            console.log("error getting books from DB", err)
+            console.log("error getting book details from DB", err)
             next(err);
         });
-  
 })
+
 
 
 module.exports = router;
